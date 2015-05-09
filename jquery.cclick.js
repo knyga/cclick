cd1d98de83714ef41ecf578a898e5752ba87d45c
knyga <oleksandrknyga@gmail.com>
2015-05-09 02:43:28 +0200

    Update jquery.cclick.js

--- jquery.cclick.js
+++ jquery.cclick.js
@@ -1,37 +1,38 @@
 (function($) {
 
-	var getOS = function() {
-		var OSName="Unknown OS";
+    var getOS = function() {
+        var OSName="Unknown OS";
 
-		if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-		if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-		if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-		if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+        if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+        if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+        if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+        if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
 
-		return OSName;
-	};
+        return OSName;
+    };
 
-	var os = getOS();
-		ctrlDown = false;
+    var os = getOS();
 
-	$(document).keydown(function(event) {
-		if ((event.keyCode == 17 && os != 'MacOS') || (event.keyCode == 91)) {
-			ctrlDown = true;
-		}
-	}).keyup(function(event) {
-		if ((event.keyCode == 17 && os != 'MacOS') || (event.keyCode == 91)) {
-			ctrlDown = false;
-		}
-	});
+    $(document).keydown(function(event) {
+        if ((event.keyCode == 17 && os != 'MacOS') || (event.keyCode == 91)) {
+            $.isCtrlDown = true;
+        }
+    }).keyup(function(event) {
+        if ((event.keyCode == 17 && os != 'MacOS') || (event.keyCode == 91)) {
+            $.isCtrlDown = false;
+        }
+    });
 
-	$.fn.cclick = function(fn) {
-		return this.each(function() {
-			$(this).on('click', function() {
-				if(ctrlDown) {
-					fn.apply(this, arguments);
-				}
-			})
-		});
-	};
+    $.fn.cclick = function(fn) {
+        return this.each(function() {
+            $(this).on('click', function() {
+                if($.isCtrlDown) {
+                    fn.apply(this, arguments);
+                }
+            });
+        });
+    };
+
+    $.isCtrlDown = false;
 
 })(jQuery)
